68b93ca76f826ee362b58745e8e93591979a6b7e
Satoh <hisatom0907@gmail.com>
2024-11-17 23:35:33 +0900

    login.featureのexampleをJSON形式に変更

--- tests/features/login.feature
+++ tests/features/login.feature
@@ -3,15 +3,17 @@ Feature: HOTEL_PLANISPHEREログイン
   Scenario Outline: login
     Given HOTELPLANISPHEREのホームページにアクセスする
     When ログインボタンを押下する
-    And email、パスワード欄に「<email>」、「<password>」と入力しログインボタンを押下する
+    And ログイン画面で「<login_input>」を入力しログインボタンを押下する
     Then ページの見出しが「マイページ」であることを確認する
-    And メールアドレスが「<email>」であることを確認する
-    And 氏名が「<name>」であることを確認する
-    And 会員ランクが「<rank>」であることを確認する  
+    And マイページの各項目が「<login_validate>」であることを確認する
+    And マイページをログアウトする
+    #And メールアドレスが「<email>」であることを確認する
+    #And 氏名が「<name>」であることを確認する
+    #And 会員ランクが「<rank>」であることを確認する  
 
     Examples:
-    | name | email | password | rank |  
-    | 山田一郎 | ichiro@example.com | password | プレミアム会員 |  
-    | 松本さくら | sakura@example.com | pass1234 | 一般会員 |  
-    | 林潤 | jun@example.com | pa55w0rd! | プレミアム会員 |  
-    | 木村良樹 | yoshiki@example.com | pass-pass | 一般会員 |  
+    | login_input | login_validate |  
+    | {"ログイン情報": {"email": "ichiro@example.com", "password": "password"}} | {"マイページ情報":{"email": "ichiro@example.com" ,"name": "山田一郎" ,"rank": "プレミアム会員"}} | 
+    | {"ログイン情報": {"email": "sakura@example.com", "password": "pass1234"}} | {"マイページ情報":{"email": "sakura@example.com" ,"name": "松本さくら" ,"rank": "一般会員"}} | 
+    | {"ログイン情報": {"email": "jun@example.com", "password": "pa55w0rd!"}} | {"マイページ情報":{"email": "jun@example.com" ,"name": "林潤" ,"rank": "プレミアム会員"}} | 
+    | {"ログイン情報": {"email": " yoshiki@example.com", "password": "pass-pass"}} | {"マイページ情報":{"email": " yoshiki@example.com" ,"name": "木村良樹" ,"rank": "一般会員"}} | 
--- tests/steps/test_login.py
+++ tests/steps/test_login.py
@@ -1,12 +1,9 @@
-import re
-
-
 from pages.home import HomePage
 from pages.login import LoginPage
 from pages.mypage import MyPage
-
-from playwright.sync_api import Page,expect
+from playwright.sync_api import expect
 from pytest_bdd import scenarios, given, when, then, parsers
+import json
 
 # ガーキンファイルの読み込み
 scenarios('login.feature')
@@ -19,25 +16,30 @@ def step_given(home_page: HomePage):
 def step_when_login(home_page: HomePage):
     home_page.click_login()
 
-@when(parsers.parse('email、パスワード欄に「{email}」、「{pwd}」と入力しログインボタンを押下する'))
-def step_when_login(login_page:LoginPage, email, pwd):
+@when(parsers.parse('ログイン画面で「{login_input}」を入力しログインボタンを押下する'))
+def step_when_login(login_page:LoginPage, login_input):
+    login_input_list = json.loads(login_input)
+    email = login_input_list['ログイン情報']['email']
+    pwd = login_input_list['ログイン情報']['password']
     login_page.submit_login(email,pwd)
 
 @then(parsers.parse('ページの見出しが「{heading}」であることを確認する'))
 def step_then(my_page:MyPage, heading):
     expect(my_page.mypage_heading).to_have_text(heading)
 
-@then(parsers.parse('メールアドレスが「{email}」であることを確認する'))
-def step_then(my_page:MyPage, email):
-        expect(my_page.email_text).to_have_text(email)
-
-@then(parsers.parse('氏名が「{name}」であることを確認する'))
-def step_then(my_page:MyPage, name):
+@then(parsers.parse('マイページの各項目が「{login_validate}」であることを確認する'))
+def step_then(my_page:MyPage, login_validate):
+    login_validate_list = json.loads(login_validate)
+    email = login_validate_list['マイページ情報']['email']
+    name = login_validate_list['マイページ情報']['name']
+    rank = login_validate_list['マイページ情報']['rank']
+    expect(my_page.email_text).to_have_text(email)
     expect(my_page.username_text).to_have_text(name)
-
-@then(parsers.parse('会員ランクが「{rank}」であることを確認する'))
-def step_then(my_page:MyPage, rank):
     expect(my_page.rank_text).to_have_text(rank)
+
+@then('マイページをログアウトする')
+def step_then(my_page:MyPage):
+    my_page.click_logout()
 
 
 
