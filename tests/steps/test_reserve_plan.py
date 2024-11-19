4fedf22ab71f436617c6752badd4942d27df07e7
Satoh <hisatom0907@gmail.com>
2024-11-19 23:53:48 +0900

    不要なコメントの削除

--- tests/steps/test_reserve_plan.py
+++ tests/steps/test_reserve_plan.py
@@ -80,42 +80,6 @@ def step_when(reserve_page:ReservePage, reserve_input):
     reserve_page.controll_rsc_checkbox(flag_reasnable_sightseeing)
     reserve_page.select_contact(reserve_contact)
 
-""" @when('宿泊日欄に明日の日付を入力する')
-def step_when(reserve_page:ReservePage):
-    reserve_page = ReservePage(pytest.new_page)
-    reserve_page.click_tomorrow()
-    reserve_page.controll_mvc_checkbox(flag_morning)
-
-@when(parsers.parse('宿泊数欄に「{stay_num}」を入力する'))
-def step_when(reserve_page:ReservePage, stay_num):
-    reserve_page = ReservePage(pytest.new_page)
-    reserve_page.fill_term(stay_num)
-
-@when(parsers.parse('人数欄に「{people_num}」を入力する'))
-def step_when(reserve_page:ReservePage, people_num):
-    reserve_page = ReservePage(pytest.new_page)
-    reserve_page.fill_head_count(people_num)
-
-@when(parsers.parse('朝食プランのチェックボックスを「{flag_morning}」にする'))
-def step_when(reserve_page:ReservePage, flag_morning):
-    reserve_page = ReservePage(pytest.new_page)
-    reserve_page.controll_mvc_checkbox(flag_morning)
-
-@when(parsers.parse('昼からチェックインプランのチェックボックスを「{flag_noon_checkin}」にする'))
-def step_when(reserve_page:ReservePage, flag_noon_checkin):
-    reserve_page = ReservePage(pytest.new_page)
-    reserve_page.controll_ncc_checkbox(flag_noon_checkin)
-
-@when(parsers.parse('お得な観光プランのチェックボックスを「{flag_reasnable_sightseeing}」にする'))
-def step_when(reserve_page:ReservePage, flag_reasnable_sightseeing):
-    reserve_page = ReservePage(pytest.new_page)
-    reserve_page.controll_rsc_checkbox(flag_reasnable_sightseeing)
-
-@when(parsers.parse('確認のご連絡リストを「{confirm_contact}」に選択する'))
-def step_when(reserve_page:ReservePage, confirm_contact):
-    reserve_page = ReservePage(pytest.new_page)
-    reserve_page.select_contact(confirm_contact) """
-
 @when(parsers.parse('合計欄が「{reserve_total_bill}」であることを確認する'))
 def step_when(reserve_page:ReservePage, reserve_total_bill):
     # ページインスタンスの引き継ぎ  
@@ -167,44 +131,6 @@ def step_then(confirm_page:ConfirmPage, confirm_validate):
 
     expect(confirm_page.contact).to_have_text(confirm_contact)
     expect(confirm_page.comment).to_have_text(comment)
-
-
-""" @then(parsers.parse('合計金額が「{total_bill}」であることを確認する'))
-def step_then(confirm_page:ConfirmPage, total_bill):
-    confirm_page = ConfirmPage(pytest.new_page)
-    expect(confirm_page.total_bill).to_contain_text(total_bill)
-
-@then(parsers.parse('プラン名が「{plan_name}」であることを確認する'))
-def step_then(confirm_page:ConfirmPage, plan_name):
-    confirm_page = ConfirmPage(pytest.new_page)
-    expect(confirm_page.plan_name).to_have_text(plan_name)
-
-@then(parsers.parse('期間が 今日から「{stay_num}」を足した日までであることを確認する'))
-def step_then(confirm_page:ConfirmPage, stay_num):
-    confirm_page = ConfirmPage(pytest.new_page)
-    stay_term = confirm_page.calc_term(stay_num)
-    expect(confirm_page.term).to_have_text(stay_term)
-
-@then(parsers.parse('追加プランが「{additional_plan}」であることを確認する'))
-def step_then(confirm_page:ConfirmPage, additional_plan):
-    confirm_page = ConfirmPage(pytest.new_page)
-    expect(confirm_page.plans).to_have_text(additional_plan)
-
-@then(parsers.parse('お名前が「{name}様」であることを確認する'))
-def step_then(confirm_page:ConfirmPage, name):
-    confirm_page = ConfirmPage(pytest.new_page)
-    validate_name = name + "様"
-    expect(confirm_page.username).to_have_text(validate_name)
-
-@then(parsers.parse('確認のご連絡が「{confirm_contact}」であることを確認する'))
-def step_then(confirm_page:ConfirmPage, confirm_contact):
-    confirm_page = ConfirmPage(pytest.new_page)
-    expect(confirm_page.contact).to_have_text(confirm_contact)
-
-@then(parsers.parse('ご要望・ご連絡事項等が「{comment}」であることを確認する'))
-def step_then(confirm_page:ConfirmPage, comment):
-    confirm_page = ConfirmPage(pytest.new_page)
-    expect(confirm_page.comment).to_have_text(comment) """
 
 @then('この内容で予約するボタンを押下する')
 def step_then(confirm_page:ConfirmPage):
